552dd25d87415f216007092776f1d961c5844a63
Nancy <nwu928@aucklanduni.ac.nz>
2023-06-15 02:28:31 +1200

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: [-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+        "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+        sans-serif],
+        code: ["source-code-pro", "Menlo", "Monaco", "Consolas", "Courier New", monospace],
+      },
+      colors: {
+        'primary': '#3d5634',
+      }
+    },
+  },
+  plugins: [],
+}
+
